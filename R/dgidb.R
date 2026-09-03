cb5ac230bc6ea2744841faa60350677536090152
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-09-03 16:14:50 -0400

    style: fix formatting weirdness (maybe)

--- R/dgidb.R
+++ R/dgidb.R
@@ -92,10 +92,12 @@
 #' @examplesIf interactive()
 #' getDrugs("Imatinib")
 #' @export
-getDrugs <- function(terms,
-    immunotherapy = NULL,
-    antineoplastic = NULL,
-    apiUrl = NULL) {
+getDrugs <- function(
+  terms,
+  immunotherapy = NULL,
+  antineoplastic = NULL,
+  apiUrl = NULL
+) {
     params <- list(names = terms)
     if (!is.null(immunotherapy)) params$immunotherapy <- immunotherapy
     if (!is.null(antineoplastic)) params$antiNeoplastic <- antineoplastic
@@ -236,15 +238,17 @@ getGenes <- function(terms, apiUrl = NULL) {
 #'     apiUrl = NULL
 #' )
 #' @export
-getInteractions <- function(terms,
-    search = "genes",
-    immunotherapy = NULL,
-    antineoplastic = NULL,
-    source = NULL,
-    pmid = NULL,
-    interactionType = NULL,
-    approved = NULL,
-    apiUrl = NULL) {
+getInteractions <- function(
+  terms,
+  search = "genes",
+  immunotherapy = NULL,
+  antineoplastic = NULL,
+  source = NULL,
+  pmid = NULL,
+  interactionType = NULL,
+  approved = NULL,
+  apiUrl = NULL
+) {
     search <- match.arg(search, c("genes", "drugs"))
     params <- list(names = terms)
     if (!is.null(source)) params$sourceDbName <- source
